f66c448cdab976b1212668473756470412800571
Tim Buchwaldt <tim@0bs.tech>
2019-12-22 18:09:45 +0100

    Add sample logic with fun_with_flags

--- lib/stacker_web/controllers/page_controller.ex
+++ lib/stacker_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule StackerWeb.PageController do
   use StackerWeb, :controller
 
   def index(conn, _params) do
+    if FunWithFlags.enabled?(:new_greeting) do
+      IO.puts("New greeting would be on")
+    end
+
     render(conn, "index.html")
   end
 end
