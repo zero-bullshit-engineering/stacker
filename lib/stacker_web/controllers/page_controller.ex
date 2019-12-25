d344a618dda166a2082217ca118bc04bf369fb89
Tim Buchwaldt <tim@0bs.tech>
2019-12-25 15:38:12 +0100

    Enable rate limiting

--- lib/stacker_web/controllers/page_controller.ex
+++ lib/stacker_web/controllers/page_controller.ex
@@ -1,6 +1,11 @@
 defmodule StackerWeb.PageController do
   use StackerWeb, :controller
 
+  plug ZerobsStack.RateLimitPlug, %ZerobsStack.RateLimitPlug{
+    name: "PageController",
+    identifier: [:private, :phoenix_format]
+  }
+
   def index(conn, _params) do
     if FunWithFlags.enabled?(:new_greeting) do
       IO.puts("New greeting would be on")
