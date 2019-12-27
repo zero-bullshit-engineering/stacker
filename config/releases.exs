5b202b2ceceffca5e1e7cf2636a7c5ac9a27fa16
Tim Buchwaldt <tim@0bs.tech>
2019-12-27 11:49:03 +0100

    Move prod.secret.exs fully over

--- config/releases.exs
+++ config/releases.exs
@@ -1,9 +1,5 @@
 import Config
 
-config :stacker, StackerWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
-
 database_url =
   System.get_env("DATABASE_URL") ||
     raise """
@@ -14,4 +10,15 @@ database_url =
 config :stacker, Stacker.Repo,
   # ssl: true,
   url: database_url,
-  pool_size: 10
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :stacker, StackerWeb.Endpoint,
+  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  secret_key_base: secret_key_base
